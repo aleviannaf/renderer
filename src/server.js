f84f117f7393eef5da835f8d342e8ca2def65637
aleviannaf <alexandrevianna511@gmail.com>
2023-10-26 13:31:04 -0400

    feat: created data with readable stream

--- src/server.js
+++ src/server.js
@@ -1,9 +1,24 @@
+import { randomUUID } from 'crypto'
 import http from 'http'
 import { Readable } from 'stream'
+
+function * run () {
+    for(let index = 0; index <=99; index ++){
+        const data ={
+            id: randomUUID(),
+            name: `Alexandre-${index}`
+        }
+        yield data
+    }
+}
 
 async function handler(request, response){
     const readable =new Readable({
         read(){
+            for(const data of run()){
+                console.log(`sending `, data)
+                this.push(JSON.stringify(data)+ "\n")
+            }
             // para informar que os dados acabaram
             this.push(null)
         }
